55e44574670c1dfb6d944a77c4c4cca3efd775da
Eric Beuque <eric.beuque@gmail.com>
2022-10-06 14:36:52 +0200

    Fix member form

--- src/Controller/Admin/CotisationCrudController.php
+++ src/Controller/Admin/CotisationCrudController.php
@@ -3,7 +3,6 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Cotisation;
-use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
@@ -29,12 +28,5 @@ class CotisationCrudController extends AbstractCrudController
             DateField::new('endDate'),
             AssociationField::new('fiscalYear'),
         ];
-    }
-
-    public function persistEntity(EntityManagerInterface $em, $entityInstance): void
-    {
-        if(!$entityInstance instanceof Cotisation) return;
-        //$entityInstance->setCreatedAt(new \DateTimeImmutable);
-        parent::persistEntity($em, $entityInstance);
     }
 }
--- src/Controller/Admin/MemberCrudController.php
+++ src/Controller/Admin/MemberCrudController.php
@@ -3,7 +3,17 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Member;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 
 class MemberCrudController extends AbstractCrudController
 {
@@ -12,14 +22,48 @@ class MemberCrudController extends AbstractCrudController
         return Member::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('lastname'),
+            TextField::new('firstname'),
+            ChoiceField::new('gender')->setChoices([
+                'Unknown' => 0,
+                'Male' => 1,
+                'Female' => 2,
+            ]),
+            DateField::new('birthdate'),
+            TextField::new('address'),
+            IntegerField::new('zipcode'),
+            TextField::new('city'),
+            EmailField::new('email'),
+            TextField::new('phonenumber'),
+            TextField::new('phonenumber2'),
+            BooleanField::new('isMember'),
+            BooleanField::new('allowImageRights'),
+            TextEditorField::new('comments'),
+            DateTimeField::new('createdAt')->hideOnForm(),
+            DateTimeField::new('updatedAt')->hideOnForm(),
         ];
     }
-    */
+
+    public function updateEntity(EntityManagerInterface $em, $entityInstance): void
+    {
+        if(!$entityInstance instanceof Member) return;
+
+        $dateNow = new \DateTimeImmutable;
+        $entityInstance->setUpdatedAt($dateNow);
+        parent::persistEntity($em, $entityInstance);
+    }
+
+    public function persistEntity(EntityManagerInterface $em, $entityInstance): void
+    {
+        if(!$entityInstance instanceof Member) return;
+
+        $dateNow = new \DateTimeImmutable;
+        $entityInstance->setCreatedAt($dateNow);
+        $entityInstance->setUpdatedAt($dateNow);
+        parent::persistEntity($em, $entityInstance);
+    }
 }
